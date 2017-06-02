01deb2cff2e3b119efa9f2cbcc45d9551e8c3c9a
Oliver Breuer <oliver.breuer@jsperts.de>
2017-06-02 07:50:58 +0200

    es6 - Unterstützung redux dev tools extension

--- es6/env_browser/client/store/createStore.js
+++ es6/env_browser/client/store/createStore.js
@@ -7,6 +7,15 @@ function createStore({
   return reduxCreateStore(
     rootReducer,
     initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
+      serialize: {
+        // allow symbols als action keys (normally we should avoid them because they're not serializable)
+        replacer: (key, value) => {
+          if (typeof value === 'symbol') return String(value)
+          return value
+        },
+      },
+    }),
   )
 }
 
--- es6/env_browser/client/util/createRootReducer.js
+++ es6/env_browser/client/util/createRootReducer.js
@@ -1,10 +1,17 @@
 function createRootReducer(eventHandlers) {
   return (prevState, event) => {
+    if (event.type === '@@INIT') {
+      // ignore
+      return prevState
+    }
     if (!eventHandlers.hasOwnProperty(event.type)) {
       console.error('unknown event', event.type)
       throw new Error('Unknown event/no event handler for: ' + event.type.toString())
     }
-    const newState = eventHandlers[event.type](prevState, event.value)
+    // clone the previous state to allow the reducer to change the state and still have diffs in the dev tools
+    // normally the reducer shouldn't change the state but we allow this here to focus on the ES6 code in the excercises
+    const prevStateClone = JSON.parse(JSON.stringify(prevState))
+    const newState = eventHandlers[event.type](prevStateClone, event.value)
     if (newState === undefined) {
       throw new Error('Event handler returned undefined instead of the new state. Did you forget a return statement?')
     }
