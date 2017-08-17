753ba1b81b28a28cfa3977a89e194346f20e172b
Nikolas Poniros <nikolas.poniros@jsperts.de>
2017-08-17 20:17:29 +0200

    Remove unused import

--- angular/examples/Unit_Tests/src/app/send_to_server.service.spec.ts
+++ angular/examples/Unit_Tests/src/app/send_to_server.service.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
